8797f02498e442bb270bb8a3a384b1d240ade2e8
Anchal2003 <sahuanchal22@gmail.com>
2025-02-21 23:38:46 +0530

    [Anchal Sahu] Add Welcome file.

---
+++ WelcomeToSpring.java
@@ -0,0 +1,7 @@
+//Create a WelcomeToSpring to print the welcome statement
+public class WelcomeToSpring {
+    public static void main(String[] args) {
+        System.out.println("\"Welcome to the Spring!\"");
+    }
+
+}
