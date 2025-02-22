e8837fee1273c93c2fef14e7f69297816199a1f1
Anchal2003 <sahuanchal22@gmail.com>
2025-02-22 11:47:34 +0530

    [Anchal Sahu] Add UC5- Make Rest call by put requeset.

--- WelcomeToSpring.java
+++
@@ -1,7 +0,0 @@
-//Create a WelcomeToSpring to print the welcome statement
-public class WelcomeToSpring {
-    public static void main(String[] args) {
-        System.out.println("\"Welcome to the Spring!\"");
-    }
-
-}
